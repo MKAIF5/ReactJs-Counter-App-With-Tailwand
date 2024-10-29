bf4edbdfa9eb2ea2a785f8f885908c9de2bca976
mohammed kaif <mkaif555123@gmail.com>
2024-10-29 17:52:59 +0500

    20% Work Completed

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -5,10 +5,16 @@ function Counter() {
         <>
 
             <div className='flex justify-center items-center h-full'>
-                <div className='p-6 bg-white w-48'>
+                <div className='p-6 bg-white w-96 rounded-3xl'>
 
                     <div>
-                        <h1 className='text-center'>Counter App</h1>
+                        <h1 className='text-center text-3xl text-yellow-400
+                        font-mono font-semibold'>Counter App</h1>
+                    </div>
+                    <br /><br />
+                    <div className='flex justify-center'>
+                        <button className='bg-yellow-400 text-white
+                        p-2 w-36 rounded-full'>Add</button>
                     </div>
                 </div>
             </div>
