41e2c26d1ef03d1cf312ace012045a6d7fe9369c
mohammed kaif <mkaif555123@gmail.com>
2024-10-29 19:30:26 +0500

    80% Work Completed

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,38 +1,48 @@
+import { useState } from "react"
 import Switcher from "./Switcher"
 
 function Counter() {
-    return (
-        <>
 
-            <div className='flex justify-center items-center h-full'>
-                <div className='p-6 bg-white w-96 rounded-3xl'>
+    const [num, setNum] = useState(1);
 
-                    <div>
-                        <h1 className='text-center text-3xl text-yellow-400
+
+
+        return (
+            <>
+
+                <div className='flex justify-center items-center h-full'>
+                    <div className='p-6 bg-white w-96 rounded-3xl'>
+
+                        <div>
+                            <h1 className='text-center text-3xl text-yellow-400
                         font-mono font-semibold'>Counter App</h1>
-                    </div>
-                    <br /><br />
-                    <div className='flex justify-center'>
-                        <button className='bg-yellow-400 text-white
-                        p-2 w-36 rounded-full'>Add</button>
-                    </div>
-                    <br />
-                    <div>
-                        <p className='text-center'>0</p>
-                    </div>
-                    <br />
-                    <div className='flex justify-center'>
-                        <button className='bg-yellow-400 text-white
-                        p-2 w-36 rounded-full'>Sub</button>
-                    </div>
-                    <br /><br />
-                    <div>
-                        <Switcher />
+                        </div>
+                        <br /><br />
+                        <div className='flex justify-center'>
+                            <button className='bg-yellow-400 text-white
+                        p-2 w-36 rounded-full'
+                                onClick={() => {
+                                    setNum(num + 1)
+                                }}>Add</button>
+                        </div>
+                        <br />
+                        <div>
+                            <p className='text-center'>{num}</p>
+                        </div>
+                        <br />
+                        <div className='flex justify-center'>
+                            <button className='bg-yellow-400 text-white
+                        p-2 w-36 rounded-full'
+                                onClick={() => { setNum(num - 1) }}>Sub</button>
+                        </div>
+                        <br /><br />
+                        <div>
+                            <Switcher />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
 }
 
 export default Counter
